06a55b31f99698ba477efb0965c1e8781b5ab7a2
Ayslan Gadelha <ayslan.github2024@gmail.com>
2024-10-01 17:07:44 -0300

    Classe SekkerDaoJDBC e Fábrica Dao

--- demo-dao-jdbc/src/Main.java
+++ demo-dao-jdbc/src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,7 @@ public class Main {
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
 
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
         
     }
